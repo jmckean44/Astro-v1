3bd34813f927359cb013d7bf215886c707efcc0e
Jeff McKean <jmckean44@gmail.com>
2024-01-24 18:38:09 -0500

    fourth commit

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,15 +2,13 @@ const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.nav-links');
 const overlay = document.querySelector('.overlay');
 
-document.addEventListener('astro:page-load', () => {
-	hamburger.addEventListener('click', () => {
-		hamburger.classList.toggle('is-active');
-		mobileNav.classList.toggle('active');
-		overlay.classList.toggle('open');
-	});
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('is-active');
+	mobileNav.classList.toggle('active');
+	overlay.classList.toggle('open');
 });
 
-document.addEventListener('astro:after-swap', () => ({}));
+
 
 // Disable scroll-to-top default behaviour
 // document.addEventListener('astro:after-swap', () =>
