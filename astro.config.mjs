83cfbae2d0b3b6a8d27cb377cb9bfd114713d033
Jeff McKean <jmckean44@gmail.com>
2024-01-27 16:06:19 -0500

    Commit UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static', // hybrid, server, static = pre-rendered
+	output: 'hybrid', // hybrid, server, static = pre-rendered pages
 	prefetch: true,
 	integrations: [mdx(), sitemap()],
+	adapter: netlify(),
 });
